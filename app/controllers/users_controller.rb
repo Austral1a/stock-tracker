ded2908b48efcebe408bc4351030f6df115da425
Austral1a <lienglex@gmail.com>
2020-05-04 19:46:21 +0300

    add friends list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,7 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks # current_user are provided by devise gem
   end
+  def my_friends
+    @tracked_friends = current_user.friends
+  end
 end
